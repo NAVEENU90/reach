ff35677143174c93f1656948726381c8a402fd16
Naveen U <153796634+NAVEENU90@users.noreply.github.com>
2026-08-16 12:55:56 +0530

    Implement authentication routes and middleware for user verification

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,9 @@ import { clerkMiddleware } from "@clerk/express";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js  ";
 import job from "./lib/cron.js";
+
 import clerkWebhook from './webhooks/clerk.webhook.js'
+import authRoutes from './routes/auth.route.js'
 
 const app = express();
 
@@ -28,6 +30,8 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+app.use("/api/auth",authRoutes)
 
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
