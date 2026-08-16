33c5b4a20837fba6e9c3cdec886c9043d07da25b
Naveen U <153796634+NAVEENU90@users.noreply.github.com>
2026-08-16 14:01:51 +0530

    Add message handling routes and media upload functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ import job from "./lib/cron.js";
 
 import clerkWebhook from './webhooks/clerk.webhook.js'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/messge.route.js'  
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/api/auth",authRoutes)
+app.use("/api/messages", messageRoutes)
 
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
